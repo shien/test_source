40d7338175cf6befdd2df25e9e4d36a9cfc5ab29
shien <sienrizumu@gmail.com>
2017-06-08 08:18:23 +0900

    test go routine.

--- golang/test.go
+++ golang/test.go
@@ -2,19 +2,23 @@ package main
 
 import "fmt"
 import "strconv"
+import "time"
 
 func main() {
-    fizzBuzz()
+    go fizzBuzz(1)
+    go fizzBuzz(2)
+    go fizzBuzz(3)
+    time.Sleep(5 * time.Millisecond)
 }
 
-func fizzBuzz() {
+func fizzBuzz(num int) {
     for i := 0; i < 100 ; i++ {
         if i % (3 * 5) == 0 {
-            fmt.Println(strconv.Itoa(i) + ": FizzBuzz")
+            fmt.Println(strconv.Itoa(i) + ": FizzBuzz -> " + strconv.Itoa(num))
         } else if i % 5 == 0 {
-            fmt.Println(strconv.Itoa(i) + ": Fizz")
+            fmt.Println(strconv.Itoa(i) + ": Fizz -> " + strconv.Itoa(num))
         } else if i % 3 == 0 {
-            fmt.Println(strconv.Itoa(i) + ": Buzz")
+            fmt.Println(strconv.Itoa(i) + ": Buzz -> " + strconv.Itoa(num))
         }
     }
 }
