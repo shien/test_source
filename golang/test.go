21cabc3c87f3107173ea25d8c4f7a7a7182315db
shien <sienrizumu@gmail.com>
2017-06-07 12:46:54 +0900

    test

--- golang/test.go
+++ golang/test.go
@@ -3,11 +3,17 @@ package main
 import "fmt"
 
 func main() {
+    go fizzBuzz()
+}
+
+func fizzBuzz() {
     for i := 0; i < 100 ; i++ {
-        if i % 2 == 0 {
-            fmt.Println("fizz")
+        if i % (2 * 5) == 0 {
+            fmt.Println(fmt.Sprint(i) + "FizzBuzz")
         } else if i % 5 == 0 {
-            fmt.Println("buzz")
+            fmt.Println("Fizz")
+        } else if i % 2 == 0 {
+            fmt.Println("Buzz")
         }
     }
 }
