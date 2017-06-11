d9bf5afea3f7f241ffd091419721aa4bcacc8458
shien <sienrizumu@gmail.com>
2017-06-11 23:12:24 +0900

    using channel for sending and reciving.

---
+++ golang/test_channel.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+import "strconv"
+import "time"
+
+func main() {
+
+    c := make(chan int)
+    go fizzBuzz(2, c)
+    defer close(c)
+
+    var i = <- c
+
+    time.Sleep(5 * time.Millisecond)
+    fmt.Println(strconv.Itoa(i))
+}
+
+func fizzBuzz(num int, s chan<- int) {
+    for i := 0; i < 100 ; i++ {
+        if i % (3 * 5) == 0 {
+            fmt.Println(strconv.Itoa(i) + ": FizzBuzz -> " + strconv.Itoa(num))
+        } else if i % 5 == 0 {
+            fmt.Println(strconv.Itoa(i) + ": Fizz -> " + strconv.Itoa(num))
+        } else if i % 3 == 0 {
+            fmt.Println(strconv.Itoa(i) + ": Buzz -> " + strconv.Itoa(num))
+        }
+    }
+    s <- num
+}
